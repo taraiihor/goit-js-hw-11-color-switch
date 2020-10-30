2183a5ae8660ae662c04a7b590870bdf83905a08
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-10-30 20:47:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,20 +13,34 @@ const refs = {
   body: document.body,
 };
 
-refs.startBtn.addEventListener('click', onStartTime);
-refs.stopBtn.addEventListener('click', offStartTime);
+const randomBodyColor = {
+  intervalId: null,
+  isActiv: false,
 
-function onStartTime() {
-  intervalId = setInterval(() => {
-    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
-  }, 1000);
-}
+  start() {
+    if (this.isActiv) {
+      return;
+    }
+    this.isActiv = true;
+    this.intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+    }, 1000);
+  },
 
-function offStartTime() {
-  clearInterval(intervalId);
-  console.log('виключити');
-}
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActiv = false;
+  },
+};
 
+refs.startBtn.addEventListener('click', () => {
+  randomBodyColor.start();
+});
+refs.stopBtn.addEventListener('click', () => {
+  randomBodyColor.stop();
+});
+
+// метод рандмного вибору
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
